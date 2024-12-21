e33806fe15221c287928d636ea566349b6d6c1f0
Sjoerd Visscher <sjoerd.visscher@tweag.io>
2024-12-21 13:11:56 +0100

    Day 21

--- aoc20.hs
+++ aoc20.hs
@@ -1,17 +1,14 @@
 import qualified Data.HashSet as S
 import Data.Graph.AStar (aStar)
-import Data.List (findIndex, elemIndex)
-import Data.Maybe (fromMaybe, fromJust)
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = interact (show . val . lines)
 
 val :: [[Char]] -> Int
-val m = length $ [ () | (i0, (x0, y0)) <- path, (i1, (x1, y1)) <- drop (i0 + 100) path, let l = abs (x1 - x0) + abs (y1 - y0), l <= 20, i1 - i0 - l >= 100]
+val m = length $ [ () | (i0, (x0, y0)) <- path, (i1, (x1, y1)) <- drop (i0 + 100) path, let d = abs (x1 - x0) + abs (y1 - y0), d <= 20, i1 - i0 - d >= 100]
   where
-    path = zip [0..] $ ((sx,sy):) $ fromJust $ aStar neighbors (\_ _ -> 1) (\_ -> 0) (\(x, y) -> m !! y !! x == 'E') (sx, sy)
-    sy = fromMaybe (-1) $ findIndex (elem 'S') m
-    sx = fromMaybe (-1) $ elemIndex 'S' (m !! sy)
+    path = zip [0..] $ (start:) $ fromJust $ aStar neighbors (\_ _ -> 1::Int) (\_ -> 0) (\(x, y) -> m !! y !! x == 'E') start
+    start = [ (x, y) | (y, row) <- zip [0..] m, (x, c) <- zip [0..] row, c == 'S'] !! 0
     neighbors (x, y) = S.fromList [(x', y') | (dx, dy) <- [(0, 1), (0, -1), (1, 0), (-1, 0)],
       let x' = x + dx, let y' = y + dy, m !! y' !! x' /= '#']
-
--- aoc21.hs
+++ aoc21.hs
@@ -1,30 +1,44 @@
-{-# LANGUAGE TupleSections #-}
-
-import Data.List (findIndex, elemIndex, group)
-import Data.Maybe (fromMaybe)
-import qualified Data.Map as Map
+import qualified Data.Map as M
+import Control.Monad (replicateM)
 
 main :: IO ()
-main = interact (show . part2 . val . lines)
+main = interact (show . sum . map val . lines)
 
-val :: [[Char]] -> (Int, [Int])
-val chart =
-  let h = length chart
-      w = length (head chart)
-      sy = fromMaybe (-1) $ findIndex (elem 'S') chart
-      sx = fromMaybe (-1) $ elemIndex 'S' (chart !! sy)
-      go :: Map.Map (Int, Int) Int -> [(Int, Int, Int)] -> [Int]
-      go _ [] = []
-      go m ((_, x, y): ps) | Map.member (x, y) m || chart !! (y `mod` h) !! (x `mod` w) == '#' = go m ps
-      go m ((d, x, y): ps) = d :
-        go (Map.insert (x, y) d m) (ps ++ map (\(dx, dy) -> (d + 1, x + dx, y + dy)) [(0, 1), (1, 0), (0, -1), (-1, 0)])
-  in (h,) $ map length $ group $ go Map.empty [(0, sx, sy)]
+topLevel :: Int
+topLevel = 25
 
-part2 :: (Int, [Int]) -> Int
-part2 (h, res) =
-  let diffs = zipWith (-) (drop h res) res
-      diffs' = findCycle diffs
-      findCycle as = if take h as == take h (drop h as) then cycle (take h as) else head as : findCycle (tail as)
-      res' = take h res ++ zipWith (+) res' diffs'
-      totals = zipWith (+) res' (0:0:totals)
-  in totals !! 26501365
+val :: String -> Int
+val s = read (init s) * typ 0 s
+
+typ :: Int -> String -> Int
+typ level s = go (m M.! 'A') s
+  where
+    pad = if level == 0 then ["789", "456", "123", " 0A"] else [" ^A", "<v>"]
+    m = M.fromList $ [ (c, (x, y)) | (y, row) <- zip [0..] pad, (x, c) <- zip [0..] row ]
+    go _ [] = 0
+    go (x, y) (c:cs) = best + go (x', y') cs
+      where
+        (x', y') = m M.! c
+        repX = replicate (abs (x - x'))
+        repY = replicate (abs (y - y'))
+        forbidden = if level == 0
+          then if (0, 3) == (min x x', max y y') then
+            if x' > x then [repY 'v' ++ repX '>'] else [repX '<' ++ repY '^'] else []
+          else if (0, 0) == (min x x', min y y') then
+            if x' > x then [repY '^' ++ repX '>'] else [repX '<' ++ repY 'v'] else []
+        best = minimum
+          [ typCached (level + 1) (keys ++ "A")
+          | keys <- mix (repX (if x' > x then '>' else '<')) (repY (if y' > y then 'v' else '^'))
+          , keys `notElem` forbidden ]
+
+typCached :: Int -> String -> Int
+typCached l s | l > topLevel = length s
+typCached l s = cache M.! (l, s)
+
+cache :: M.Map (Int, [Char]) Int
+cache = M.fromList [ ((level, keys), typ level keys) | level <- [1..topLevel], len <- [0..5], keys' <- replicateM len "<>^v", let keys = keys' ++ "A" ]
+
+mix :: [a] -> [a] -> [[a]]
+mix [] ys = [ys]
+mix xs [] = [xs]
+mix (x:xs) (y:ys) = ((x :) <$> mix xs (y:ys)) ++ ((y :) <$> mix (x:xs) ys)
