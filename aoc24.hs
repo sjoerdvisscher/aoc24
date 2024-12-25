b4c083ddebceb7a0d27b21c2dad4dc821561f393
Sjoerd Visscher <sjoerd.visscher@tweag.io>
2024-12-25 09:36:52 +0100

    Day 24 and 25

--- aoc24.hs
+++ aoc24.hs
@@ -1,37 +1,40 @@
+import qualified Data.Map as M
 import Data.List.Split (splitOn)
+import Data.Bits (shiftL)
 
 main :: IO ()
-main = interact (show . part2 . map parse . lines)
+main = interact (part2 . parse)
 
-type Coord = (Integer, Integer, Integer)
-
-parse :: String -> (Coord, Coord)
-parse s = case map parseCoord (splitOn "@" s) of
-  [a, b] -> (a, b)
-
-parseCoord :: String -> Coord
-parseCoord s = case map read (splitOn "," s) of
-  [x, y, z] -> (x, y, z)
-
-hit :: (Coord, Coord) -> (Coord, Coord) -> (Integer, Integer, Coord)
-hit ((x1, y1, z1), (vx1, vy1, vz1)) ((x2, y2, z2), (vx2, vy2, vz2)) = (d, vz, (x, y, z))
+parse :: String -> (M.Map String Bool, [[String]])
+parse inp = (valMap, map words $ lines fmls)
   where
-    t2 = (y2 - y1)*vx1 - (x2 - x1)*vy1
-    t1 = (y1 - y2)*vx2 - (x1 - x2)*vy2
-    d = vy1*vx2 - vx1*vy2
-    vz = ((z2 - z1)*d + t2*vz2 + t1*vz1) `div` (t1 + t2)
-    z = z1 - t1*(vz1 - vz) `div` d
-    x = x1 - t1*vx1 `div` d
-    y = y1 - t1*vy1 `div` d
+  [vals, fmls] = splitOn "\n\n" inp
+  valMap = M.fromList $ map (\l -> case splitOn ": " l of [n, v] -> (n, v == "1")) $ lines vals
 
-part2 :: [(Coord, Coord)] -> Integer
-part2 ((p1, v1):(p2, v2):(p3, v3):_) = ax + ay + az
+part1 :: (M.Map String Bool, [[String]]) -> Int
+part1 (vals, fmls) = go 0 0
   where
-    velocities = [ v | s <- [1..], t <- [-s..s], v <- [(s, t, 0), (-s, t, 0), (t, s, 0), (t, -s, 0)]]
-    (ax, ay, az) = foldr (\v@(vx, vy, _) next ->
-      let (d12, vz, p12) = hit (p1, subCoord v1 v) (p2, subCoord v2 v)
-          v' = (vx, vy, vz)
-          (d13, vz', p13) = hit (p1, subCoord v1 v') (p3, subCoord v3 v')
-      in if d12 /= 0 && d13 /= 0 && vz' == 0 && p12 == p13 then p13 else next) (0,0,0) velocities
-    subCoord (x, y, z) (a, b, c) = (x - a, y - b, z - c)
-part2 _ = error "not enough data"
+    go n acc = maybe acc (\b -> go (n+1) (acc + (fromEnum b `shiftL` n))) $ M.lookup (z n) m
+    m = vals <> (M.fromList $ map (\[l, o, r, _, tgt] -> (tgt, op o (m M.! l) (m M.! r))) fmls)
+    op "AND" = (&&)
+    op "OR" = (||)
+    op "XOR" = (/=)
+
+sh :: Int -> String
+sh n = if n < 10 then "0" ++ show n else show n
+
+x, y, z :: Int -> String
+x n = "x" ++ sh n
+y n = "y" ++ sh n
+z n = "z" ++ sh n
+
+part2 :: (M.Map String Bool, [[String]]) -> String
+part2 (vals, fmls) = show wrongs
+  where
+    zeros = False <$ vals
+    wrongs = concatMap (\n -> if part1 (M.insert (x n) True $ M.insert (y n) True $ zeros, fmls) /= 2 `shiftL` n then [n] else []) [0..44]
+
+-- dmh, z31
+-- z11, rpv
+-- ctg, rpb
+-- dvq, z38
--- aoc25.hs
+++ aoc25.hs
@@ -1,25 +1,16 @@
 import Data.List.Split (splitOn)
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-import Math.Clustering.Spectral.Sparse (spectralClusterKNorm)
-import Data.Sparse.SpMatrix (mkSpMR)
-import Data.Sparse.SpVector (toListSV)
-import Data.List (nub)
+import Data.List (transpose)
 
 main :: IO ()
-main = interact (show . val . map parse . lines)
+main = interact (show . val . foldMap parse . splitOn "\n\n")
 
-parse :: String -> (String, [String])
-parse s = (take 3 s, splitOn " " (drop 5 s))
-
-val :: [(String, [String])] -> Int
-val adj = if length cuts == 3 then Set.size cluster * (n - Set.size cluster) else 0
+type Key = [Int]
+type Lock = [Int]
+parse :: String -> ([Lock], [Key])
+parse s = if c == '#' then ([key], []) else ([], [map (5 -) key])
   where
-    connections = concatMap (\(a, bs) -> map (\b -> (a, b)) bs) adj
-    names = nub $ map fst adj ++ concatMap snd adj
-    ix = Map.fromList $ zip names [0..]
-    nm = Map.fromList $ zip [0..] names
-    n = Map.size ix
-    adjMat = mkSpMR (n, n) $ concatMap (\(a, b) -> let ai = ix Map.! a; bi = ix Map.! b in [(ai, bi, 1), (bi, ai, 1)]) connections
-    cluster = Set.fromList $ map ((nm Map.!) . fst) $ toListSV $ spectralClusterKNorm 1 2 adjMat
-    cuts = filter (\(a, b) -> Set.member a cluster /= Set.member b cluster) connections
+    c = s !! 0
+    key = map (pred . length . takeWhile (== c)) $ transpose $ lines s
+
+val :: ([Lock], [Key]) -> Int
+val (locks, keys) = sum [ 1 | key <- keys, lock <- locks, all (<= 5) $ zipWith (+) key lock ]
